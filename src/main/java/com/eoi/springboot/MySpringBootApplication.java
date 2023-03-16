cd80cc0c1fe39be006d2a5c9ff3f9bd7b8b9938a
Moises Aguirre <aguirre.moisesdavid@gmail.com>
2023-03-16 10:05:03 +0100

    First commit -m name refactor of names and packages

---
+++ src/main/java/com/eoi/springboot/MySpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.eoi.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Haz tu magia Spring
+public class MySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+}
