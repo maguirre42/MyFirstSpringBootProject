0ef19975b8661cc20c0016afd89567144befc725
Moises Aguirre <aguirre.moisesdavid@gmail.com>
2023-03-16 13:56:46 +0100

    MSBP-5 Updated "Repositorio Usuario"

--- src/main/java/com/eoi/database/RepositorioUsuarios.java
+++ src/main/java/com/eoi/database/RepositorioUsuarios.java
@@ -1,4 +1,19 @@
-package com.eoi.database;
+package com.eoi.springboot.database;
 
-public interface RepositorioUsuarios {
+import com.eoi.springboot.entidades.EntidadUsuario;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+
+// CRUD refers Create, Read, Update, Delete
+
+public interface RepositorioUsuarios extends CrudRepository<EntidadUsuario, Integer> {
+
+
+
 }
