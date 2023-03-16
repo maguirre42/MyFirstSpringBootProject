03bf96d4da67f93e62792a5bcb310c81c914e4e3
Moises Aguirre <aguirre.moisesdavid@gmail.com>
2023-03-16 09:45:57 +0100

    Second commit -m created new class "ControladoresUsario" with code from the original "DemoApplication"

---
+++ src/main/java/com/example/demo/ControladorUsuario.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Y además preparate para recibir peticiones https
+public class ControladorUsuario {
+    @RequestMapping("/")
+    String hellow()
+    {
+        return "Hello World";
+    }
+
+    @RequestMapping("/hola")
+    String holam()
+    {
+        return "Hola Mundo";
+    }
+
+    @RequestMapping("/mimedicacion")
+    String miMedicacion()
+    {
+        return "tomate las pastillas";
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,28 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication // Haz tu magia Spring
-@RestController // Y además preparate para recibir peticiones https
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@RequestMapping("/")
-	String hellow()
-	{
-		return "Hello World";
-	}
-
-	@RequestMapping("/hola")
-	String holam()
-	{
-		return "Hola Mundo";
-	}
-
-	@RequestMapping("/mimedicacion")
-	String miMedicacion()
-	{
-		return "tomate las pastillas";
 	}
 }
